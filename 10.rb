c5ade6f5fde542f6ca8f2f2ad8b87ea944e9f298
Jury Razumau <mail@razumau.net>
2022-12-16 14:00:36 +0100

    day ten, part two

--- 10.rb
+++ 10.rb
@@ -13,11 +13,15 @@ class CPU
     @register = 1
     @waiting_to_add = nil
     @history = []
+    @pixels = []
   end
 
   def next_cycle!
     @history << @register
     @cycle_count += 1
+
+    add_pixel
+
     unless @waiting_to_add.nil?
       @register += @waiting_to_add
       @waiting_to_add = nil
@@ -28,6 +32,22 @@ class CPU
     return if command == 'noop'
 
     @waiting_to_add = command.split.last.to_i
+  end
+
+  def add_pixel
+    pixel = if (@register - ((@cycle_count - 1) % 40)).abs <= 1
+              "#"
+            else
+              "."
+            end
+    @pixels << pixel
+  end
+
+  def display_pixels
+    6.times do |index|
+      offset = index * 40
+      puts @pixels[offset...(offset+40)].join
+    end
   end
 
   def run
@@ -47,7 +67,10 @@ end
 
 def run_second(sample:)
   input = sample ? sample_input(day: DAY) : input(day: DAY)
+  cpu = CPU.new(input)
+  cpu.run
+  cpu.display_pixels
 end
 
 run_first(sample: false)
-run_second(sample: true)
+run_second(sample: false)
